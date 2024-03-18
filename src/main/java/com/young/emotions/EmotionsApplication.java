48a3a35cecefce922ce47f705e581d633ee6f886
young <eornzz0315@gmail.com>
2024-03-18 15:07:34 +0900

    first commit

---
+++ src/main/java/com/young/emotions/EmotionsApplication.java
@@ -0,0 +1,13 @@
+package com.young.emotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionsApplication.class, args);
+    }
+
+}
